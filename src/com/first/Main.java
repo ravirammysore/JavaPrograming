f2e74a0cb2462c01b6b5db31200183e30727b53d
Ravi <raviram.mysore@gmail.com>
2021-08-03 19:42:34 +0530

    session done

--- src/com/first/Main.java
+++ src/com/first/Main.java
@@ -1,15 +1,23 @@
 package com.first;
 
+import java.text.MessageFormat;
+import java.util.Calendar;
+
 public class Main {
 
     public static void main(String[] args) {
-        String name ="America";
-        boolean letterFound = false;
-        for (int i=0; i<name.length(); i++) {
-            if (name.charAt(i) == 'd') {
-                letterFound = true;
-            }
-        }
-        System.out.println("Letter found: " + letterFound);
+        String emailId = "sam@gmail.com";
+
+        int position = emailId.indexOf("@");
+        String email = emailId.substring(0, position);
+
+        String today = Calendar.getInstance().getTime().toString();
+        String msg =
+                MessageFormat.format(
+                        "Welcome to our site Mr.{0}! " +
+                                "Today is {1}, weather is {2}",
+                        email,today, 23);
+
+        System.out.println(msg);
     }
 }
